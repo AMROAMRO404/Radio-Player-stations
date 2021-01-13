8a68103a610921a278844bfa228d4470a9a43035
AMROAMRO404 <amro.amro1999@gmail.com>
2021-01-13 20:55:07 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Radio-Staions-Player';
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get(this.url).subscribe(data => {
+      this.radioObject = JSON.parse(JSON.stringify(data));
+      this.cities = this.radioObject.palestine
+      console.log(this.radioObject.palestine)
+    })
+  }
+  url = "http://www.json-generator.com/api/json/get/bIpVztBRyW?indent=2";
+  cities: []
+  radioObject: any = ''
+
+
 }
